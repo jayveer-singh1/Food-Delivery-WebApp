2aecb1271237f255da95f412085344eac4d44425
Jayveer Singh <jayveersinghrawat93512@gmail.com>
2025-07-19 14:29:48 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favorite food here</h2>
+                <p>Choose from a diverse menu featuring deletable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dinig experience, one delicious meal st s time.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
