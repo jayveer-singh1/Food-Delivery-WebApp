246b7e4d821e56a9203ce7b75f79f74489e45e6d
Jayveer Singh <jayveersinghrawat93512@gmail.com>
2025-07-20 22:04:09 +0530

    Food Display and food item added

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContext from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <StoreContext>
+      <App />
+    </StoreContext>
   </BrowserRouter>
 
 )
